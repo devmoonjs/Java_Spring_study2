a67dd0f198c15df770d134f856b6e9fc06f363a9
MOON <moonjs1018@naver.com>
2024-02-29 19:06:24 +0900

    MemberCreate 기능 추가

--- src/main/java/hello/hellospring3/controller/MemberController.java
+++ src/main/java/hello/hellospring3/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring3.controller;
 
+import hello.hellospring3.domain.Member;
 import hello.hellospring3.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -10,13 +13,22 @@ public class MemberController {
     private MemberService memberService;
 
     @Autowired
-    public void setMemberService(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
-//    @Autowired
-//    public MemberController(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
